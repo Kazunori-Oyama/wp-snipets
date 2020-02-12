1944695e1c5eb41f3c3f4fcdd791f1949518bb79
KazunoriOyama <kazunori0326.t+tonichi@gmail.com>
2020-02-13 08:53:36 +0900

    install gulp and initialized

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const {
+    src,
+    dest,
+    watch
+} = require("gulp");
+const autoprefixer = require('gulp-autoprefixer')
+const sass = require("gulp-sass");
+const gulpPostcss = require('gulp-postcss');
+const cssDeclarationSorter = require('css-declaration-sorter');
+const mmq = require('gulp-merge-media-queries');
+
+const compileSass = () =>
+    src("asset/style.scss")
+    .pipe(
+        sass({
+            outputStyle: "expanded"
+        })
+    )
+    .pipe(gulpPostcss([cssDeclarationSorter({
+        order: 'smacss'
+    })]))
+    .pipe(autoprefixer({
+
+    }))
+    .pipe(mmq({
+        log: true
+    }))
+
+    .pipe(dest("asset"));
+
+const watchSassFiles = () => watch("asset/style.scss", compileSass);
+
+exports.default = watchSassFiles;
