932febf81d8d7e9d00777422dc030dfe1ed2dd6d
KazunoriOyama <kazunori0326.t+tonichi@gmail.com>
2020-02-14 09:30:21 +0900

    re partial

--- index.php
+++ index.php
@@ -8,50 +8,55 @@
     <?php wp_head();?>  
 </head>
 <body>
-    <header class="header">
-        <div class="header__logo">
-            <img src="https://placehold.jp/250x150.png?text=logo" alt="" class="header__logo-img">
-        </div>
-        <div class="header__main-nav">
-            <?php wp_nav_menu(array('menu'=>'main-nav','container' => false,'menu_class'=>'header__main-nav')); ?>
-        </div>
-        
-    </header>
-    <div class="container">
-        <div class="row between">
-            <div class="col6">
-                <h1>みだし</h1>
-                <p>パラグラフ</p>
+    <div class="body-container">
+        <header class="header">
+            <div class="header__logo">
+                <img src="https://placehold.jp/250x150.png?text=logo" alt="" class="header__logo-img">
             </div>
-            <div class="col6">
-                <figure>
-                    <img src="https://placehold.jp/300x200.png" alt="">
-                    <figcaption>figcaption</figcaption>
-                </figure>
+            <div class="header__main-nav-wrap">
+                <?php wp_nav_menu(array('menu'=>'main-nav','container' => false,'menu_class'=>'header__main-nav')); ?>
+                <?php wp_nav_menu(array('menu'=>'second-nav','container' => false,'menu_class'=>'header__second-nav')); ?>
             </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col6">
-            <div class="row">
-                <div class="col4 col-full">content</div>
-                <div class="col4">content</div>
-                <div class="col4">content</div>
-                <div class="col4">content</div>
-                <div class="col4 col-full">content</div>
+
+        </header>
+        <div class="content">
+            <div class="container">
+                <div class="row between">
+                    <div class="col6">
+                        <h1>みだし</h1>
+                        <p>パラグラフ</p>
+                    </div>
+                    <div class="col6">
+                        <figure>
+                            <img src="https://placehold.jp/300x200.png" alt="">
+                            <figcaption>figcaption</figcaption>
+                        </figure>
+                    </div>
                 </div>
             </div>
+            <div class="row">
+                <div class="col6">
+                    <div class="row">
+                        <div class="col4 col-full">content</div>
+                        <div class="col4">content</div>
+                        <div class="col4">content</div>
+                        <div class="col4">content</div>
+                        <div class="col4 col-full">content</div>
+                        </div>
+                    </div>
+                </div>
+                </div>
+                <!-- <div class="col3">
+                    conent
+                </div>
+                <div class="col4">
+                    content
+                </div>
+                <div class="col5">
+                    content
+                </div> -->
+            </div>
         </div>
-        </div>
-        <!-- <div class="col3">
-            conent
-        </div>
-        <div class="col4">
-            content
-        </div>
-        <div class="col5">
-            content
-        </div> -->
     </div>
 </body>
 </html>
