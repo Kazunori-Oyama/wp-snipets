5220b8740fa69cc8d7221742dac38b01b948692f
KazunoriOyama <kazunori0326.t+tonichi@gmail.com>
2020-02-16 13:30:31 +0900

    justify header height with create variables

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
             
 
         </header>
-        <div style="height:1000px;">
+        <div class="content" style="height:1000px;">
             content
         </div>
         <a href="#" class="js-back-to-top"><i class="fa fa-angle-right">button</i></a>
